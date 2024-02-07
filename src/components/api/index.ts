b3d1adbdc9c3c6eb4cd12484f7ba9e588f97c031
Template1500 <template1500@proton.me>
2024-02-07 16:19:57 -0500

    FIX API Serv

--- src/components/api/index.ts
+++ src/components/api/index.ts
@@ -2,7 +2,7 @@ const API = import.meta.env.API_URL
 
 const endPoints = {
   mail: {
-    send: 'http://localhost:3001/api/v1/mail/send',
+    send: 'https://app.nacionaldecreditosalservicio.com/api/v1/mail/send',
   }
 }
 
